6649e978432cc80ab1ba8ee4dede28032c82add1
ioioio0719 <kio0719@docomo.ne.jp>
2022-04-11 18:51:34 +0900

    First committed

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Money Saving</title>
+</head>
+<body>
+	<h1>Money Saving</h1>
+	<a href="#"><p>ログイン</p></a>
+	<a href="#"><div>アカウント作成</div></a>
+</body>
+</html>
